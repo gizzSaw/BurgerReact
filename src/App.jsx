4573cd1f80c4b4e40d1d14b8a3e7e1e11c769c6d
gizzSaw <gizzsaw@gmail.com>
2023-02-23 18:56:48 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { Container } from "./components/Container/Container";
+
+export const App = () => {
+  return (
+    <>
+      <header>Савелий, здравствуйте !</header>
+      <main>
+        <nav>
+          <Container />
+        </nav>
+        <section></section>
+      </main>
+      <footer></footer>
+    </>
+  );
+};
+
+// header;
+// main;
+// nav;
+// section;
+// footer;
